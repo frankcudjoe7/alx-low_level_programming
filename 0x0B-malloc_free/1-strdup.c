14b0135b95d1819832215fa095c9c28d69983132
Frank Erzoah-Cudjoe <frankcudjoe7@gmail.com>
2023-06-13 19:16:07 -0700

    Added 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+
+char *_strdup(char *str)
+{
+	char *string;
+	unsigned int index, count = 0;
+
+	if (str == NULL)
+		return (NULL);
+	
+	while (str[count] != '\0')
+		count++;
+
+	string = malloc(sizeof(char) * count);
+
+	if (string == NULL)
+		return (NULL);
+
+	for (index = 0; index < count; index++)
+		string[index] = str[index];
+
+	return (string);
+}
